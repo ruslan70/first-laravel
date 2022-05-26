421661450c506ee054f6b1fe4b4885ddcadd506b
ruslan70 <ruslan.khatuev70@gmail.com>
2022-05-26 22:17:03 +0200

    add stings Title and content in function up()

--- database/migrations/2022_05_26_200943_create_messages_table.php
+++ database/migrations/2022_05_26_200943_create_messages_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('content');
+
             $table->timestamps();
         });
     }
